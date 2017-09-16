2e8daffa9fdc10a67d27e2969a86488eceb76487
Denis Yelchaninov <11614834+Dwarfo@users.noreply.github.com>
2017-09-16 15:36:09 +0300

    Initial Commit of whole Project

---
+++ GetOutOfMyDungeon/Assets/Test/Test_Scripts/SelectOnInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour {
+
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+    private void HandleBar()
+    {
+        buttonSelected = false;
+    }
+}
