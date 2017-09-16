f35c643ee66a85311e586c44470dcfcb86d6d1d4
Denis Yelchaninov <11614834+Dwarfo@users.noreply.github.com>
2017-09-16 15:43:16 +0300

    Initial With all metas (

---
+++ GetOutOfMyDungeon/Assets/Test/Test_Scripts/UI/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+
+    public float HP = 100;
+    public Slider HPslider;
+    public Player_Controller playerController;
+
+	// Use this for initialization
+	void Awake ()
+    {
+       // HPslider = GetComponent<Slider>();
+        //playerController = GetComponent<Player_Controller>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        HandleHP(HP);
+	}
+
+    private void HandleHP(float hp)
+    {
+        HPslider.value = (hp / 100);
+    }
+}
